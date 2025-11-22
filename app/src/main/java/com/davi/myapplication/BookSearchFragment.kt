91268cbadd6ce9873d6b578562599e98d373d655
Davi Vila Nova <davivnova28@gmail.com>
2025-11-22 19:36:49 -0300

    book register screen

--- app/src/main/java/com/davi/myapplication/BookSearchFragment.kt
+++ app/src/main/java/com/davi/myapplication/BookSearchFragment.kt
@@ -1,11 +1,12 @@
 package com.davi.myapplication
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class BookSearchFragment : Fragment() {
 
@@ -13,7 +14,17 @@ class BookSearchFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_book_search, container, false)
+    ): View {
+        // infla o layout da tela
+        val view = inflater.inflate(R.layout.fragment_book_search, container, false)
+
+        // botão "Adicionar livro"
+        val btnAddBook = view.findViewById<LinearLayout>(R.id.btn_add_book)
+        btnAddBook.setOnClickListener {
+            // navega para a tela de "Registrar Livro"
+            findNavController().navigate(R.id.action_bookSearch_to_register)
+        }
+
+        return view
     }
 }
